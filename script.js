4d53d80c4e601b4963b6ec4cbc2ac3121f24f8af
Akshaybaisware <130745994+Akshaybaisware@users.noreply.github.com>
2023-05-12 16:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
+let left=0;
+	let right=str.length-1;
 
+	while(left<right){
+		if(str[left]!=str[right])
+		{
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
 }
+
+
 module.exports = palindrome
